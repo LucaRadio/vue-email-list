c3b703d1f862ffd99af1203c9c8b12729fc5cf00
LucaRadio <luca.1407@hotmail.it>
2022-11-30 14:43:22 +0100

    Complete Exercise With bonus

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            mailListTemp: [],
+            mailList: [],
+            counter: null
+        }
+    },
+    methods: {
+
+        generateRandomMail() {
+
+            for (let i = 0; i < this.counter; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((resp) => {
+                        console.log(resp.data.response);
+                        this.mailListTemp.push(resp.data)
+
+                        if (this.mailListTemp.length === this.counter) {
+                            this.mailList = this.mailListTemp
+                            this.mailListTemp = [];
+                        };
+
+
+                    })
+
+            };
+
+
+
+
+        },
+
+    }
+
+}).mount("#app")
